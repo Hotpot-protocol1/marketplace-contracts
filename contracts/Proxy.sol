9c3aa5aaf8b45179b7c5f23d94afb1802796bd13
statskiy-sovetnik <ivangladkikh3@gmail.com>
2023-08-16 16:24:04 +0700

    fixed import

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -2,4 +2,4 @@
 pragma solidity ^0.8.19;
 
 // This is the NFTMarketplace proxy contract
-import {ERC1967Proxy} "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
