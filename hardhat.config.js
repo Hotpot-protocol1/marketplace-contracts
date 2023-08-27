536c75096360bb18ea25b3951fa3e6271b45bf20
statskiy-sovetnik <ivangladkikh3@gmail.com>
2023-08-27 11:03:06 +0700

    Added a task for verifying pending amounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 
 const {
-  INFURA_MAINNET_API, STAGE, MNEMONIC
+  INFURA_MAINNET_API, STAGE, MNEMONIC, INFURA_GOERLI_API
 } = process.env;
 
 let forking_url;
@@ -15,6 +15,40 @@ if (STAGE == "FORK_TESTING") {
   forking_url = INFURA_MAINNET_API;
   forking_block_number = 17704555;
 }
+
+task("sigverify", "Verify pending amounts", async (taskArgs, hre) => {
+  const sig = "0x6e26a1ade620b79826071d18ca5c4569bf166aca9d7cb792f70a0457a0af896823e612f11169d771fb18626dc1878334866464a7c3029c1d132beef89dde571b1c";
+  const value = {
+    offererPendingAmount: 0,
+    buyerPendingAmount: 0,
+    orderHash: "0xef5d923c3a25e3f22c219350cd9ae30b3a25e19cd471d0aa7f54112af379eea2"
+  };
+  const pendingAmountType = {
+    PendingAmountData: [
+      { name: 'offererPendingAmount', type: 'uint256' },
+      { name: 'buyerPendingAmount', type: 'uint256' },
+      { name: 'orderHash', type: 'bytes32' },
+    ],
+  };
+  const marketplace = "0x2164b06F71Ad3Ebb5AA1E31ccb3d7EF69ce8Be05";
+  const domain = {
+    name: process.env.PROTOCOL_NAME,
+    version: process.env.PROTOCOL_VERSION,
+    chainId: 5, // goerli
+    verifyingContract: marketplace
+  };
+  const domain_separator = hre.ethers.TypedDataEncoder.hashDomain(domain);
+  const pa_hash = hre.ethers.TypedDataEncoder.hash(
+    domain, pendingAmountType, value
+  );
+  console.log('Domain:', domain);
+  console.log('Domain separator: ', domain_separator);
+  console.log('Pending amount hash: ', pa_hash);
+
+  const signer = hre.ethers.verifyTypedData(domain, pendingAmountType, value, sig);
+  console.log('Operaator? ', signer);
+
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -37,6 +71,9 @@ module.exports = {
         accounts: {
           accountsBalance: "20000000000000000000000" // 20000 ETH
         }
+    },
+    goerli: {
+      url: INFURA_GOERLI_API
     }
   },
 
